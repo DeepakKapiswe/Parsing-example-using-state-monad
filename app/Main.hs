cb896c0026576f7997bb30b82f66ec59acae3993
Deepak Kapiswe <deepakkapiswe@gmail.com>
2017-07-03 21:57:55 +0530

    done with parsing string

--- app/Main.hs
+++ app/Main.hs
@@ -4,12 +4,13 @@ module Main where
 import Control.Monad
 import Control.Monad.State
 
-import Control.Applicative
+import Control.Applicative hiding (many)
 
 import SuffixLenses (suffixLenses)
 import Lens.Micro   ((^.), (&), (.~), (%~))
 
 import Data.Monoid
+import Data.Maybe
 
 import Data.Char
 import Text.ParserCombinators.ReadP hiding (get)
@@ -43,12 +44,12 @@ suffixLenses ''Person
 
 
 parseStr::[Char]->[Person]
-parseStr = u
+parseStr = getPersons.evalState transition.makeInitConf
 
 transition::State Conf Conf
 transition = do
  conf <- get
- case conf of 
+ case conf of
   Final _ -> return conf
   Inter _ st |endOfInput st -> return (Final st)
   _ -> do modify modifyConf
@@ -57,11 +58,33 @@ transition = do
 modifyConf::Conf->Conf
 modifyConf c = case c of
  Inter Init st -> case parse parseName (st ^. remainingInputL) of
-                   Right (pname,rem) -> Inter AcceptSpaces1 (st & ((remainingInputL .~ rem ).( currNameL %~ (pname <$))))
-                   Left _ -> Inter Init (removeHead st)
- Inter AcceptSpaces1 st ->  case parse parseSpaces (st ^. remainingInputL) of 
-                   Right (_,rem) -> Inter AcceptWeight (st & (remainingInputL .~ rem))
-                   Left _ -> Inter Init (gotoInitState st)
+                   Right (pname,rem) -> Inter AcceptSpaces1 $ st & 
+                                        remainingInputL .~ rem   &
+                                        currNameL .~ pure pname
+                   Left _ -> Inter Init . removeHead $ st
+ 
+ Inter AcceptSpaces1 st ->  case parse parseSpaces (st ^. remainingInputL) of
+                   Right (_,rem) -> Inter AcceptWeight $ st &
+                                    remainingInputL .~ rem
+                   Left _ -> Inter Init . gotoInitState $ st
+ 
+ Inter AcceptWeight st -> case parse parseFloat (st ^. remainingInputL) of
+                   Right (pweight,rem) -> Inter AcceptKg $
+                             st & remainingInputL .~ rem & 
+                             currWeigthL .~ pure pweight
+                   Left _ -> Inter Init . gotoInitState $ st
+
+ Inter AcceptKg st -> case parse parseKg (st ^. remainingInputL) of
+                   Right (pkg,rem)-> let cn = fromJust $ st ^. currNameL
+                                         cw = fromJust $ st ^. currWeigthL
+                                     in case rem of
+                                        [] -> Final $ st & (personsL %~ ((Person cn cw):))
+                                        (x:rest) |not (isLetter x) -> Inter Init . gotoInitState $ st &
+                                                                      (remainingInputL .~ rest).
+                                                                      (personsL %~ ((Person cn cw):))
+                                        _->Inter Init .gotoInitState $ st
+                   Left _ -> Inter Init . gotoInitState $ st
+
 
 
 getHead::Mystate->Char
@@ -71,7 +94,7 @@ removeHead::Mystate->Mystate
 removeHead =  (%~) remainingInputL (drop 1)
 
 gotoInitState::Mystate->Mystate
-gotoInitState st = st & (currNameL .~ Nothing).(currWeigthL .~ Nothing).removeHead
+gotoInitState st = st & (currNameL .~ Nothing).(currWeigthL .~ Nothing)
 
 -- | confirms whether all of the input string is consumed
 endOfInput::Mystate->Bool
@@ -84,22 +107,35 @@ parseName = many1 (satisfy isLetter)
 parseSpaces::ReadP String
 parseSpaces = many1 (char ' ')
 
+parseFloat::ReadP Float
+parseFloat = fmap read $ (++) <$> number <*> decimal
+ where
+  decimal = option "" $ (:) <$> char '.' <*> number
+  number = many1 (satisfy isDigit)
+
+parseKg::ReadP String
+parseKg = do
+     many (char ' ')
+     string "kg"
+
 
 parse::ReadP a-> String-> Either String (a,String)
-parse p s = case readP_to_S p s of 
+parse p s = case readP_to_S p s of
  [] -> Left s
  xs -> Right (last xs)
 
-u = undefined
 
-is::String->Mystate
-is i = Mystate mempty mempty Nothing i
+getPersons::Conf->[Person]
+getPersons (Final st) = st ^. personsL 
+getPersons _ = []
+
+
+u = undefined
 
 -- | makes initial configuration from a given string
 makeInitConf::String->Conf
-makeInitConf inp = Inter Init (is inp)
+makeInitConf = Inter Init . Mystate mempty mempty Nothing 
 
-iss = makeInitConf i
 
 i = "this/ si /@3 3432 djw2tony 32kg ewij#hari 75kg$jda"
 
