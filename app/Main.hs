7ab4a93cbe60689726cd88d9ba80379863a2fadc
Deepak Kapiswe <deepakkapiswe@gmail.com>
2017-07-04 17:00:01 +0530

    little modifications

--- app/Main.hs
+++ app/Main.hs
@@ -21,23 +21,35 @@ main = do
   []-> print "**exception::no argument povided atleat one required"
   xs -> mapM_ processArg xs
 
+
 processArg::FilePath->IO ()
 processArg arg = do
  recs <-fmap (personToDoc <$>) .getRecords $ arg
  runMongo dbName $ insertMany "persons" recs
 
+-- | getRecords filename loads the file and returns
+--   either [Person] or [] depending on loading of 
+--   file, It also generates a msg in case of file error
+getRecords :: FilePath -> IO [Person]
 getRecords fname = do
  content <- safeLoadFile fname 
  case parseStr <$> content of 
   Right records -> return $ validate records 
   _ -> liftIO (print $ "***error:: bad argument provided can't read file named :: "++fname) >> return mempty
 
+
+-- | converts a Person to Document Type
 personToDoc::Person->Document
 personToDoc (Person n w)= [(pack "name") := (val n),(pack "weight") := (val w)]
 
+-- | a function which will wrap reading of file in 
+--   Either so that we can check if loading of file failed
 safeLoadFile :: FilePath -> IO (Either IOException String)
 safeLoadFile f = (Right <$> readFile f) `catch` (\ e -> pure (Left e) )
 
+-- | mongoDb function runMongo takes a dbname and a function to run
+--   it opens a connection  with given host and runs that function then 
+--   closes the connection
 runMongo dbName functionToRun = do
   pipe <- connect (host "127.0.0.1")
   e <- access pipe master (pack dbName) functionToRun
--- src/Parser.hs
+++ src/Parser.hs
@@ -14,9 +14,12 @@ import Data.Maybe
 import Data.Char
 import Text.ParserCombinators.ReadP hiding (get)
 
+-- | The top level function which will parse a given
+--   string to give list of Person
 parseStr::[Char]->[Person]
 parseStr = getPersons.evalState transition.makeInitConf
 
+-- | The state monad to parse the Configuration whole transition takes place here
 transition::State Conf Conf
 transition = do
  conf <- get
@@ -26,6 +29,8 @@ transition = do
   _ -> do modify modifyConf
           transition
 
+-- | A function to modify the Configuration according to the Configuration and input string
+--   this function contains the main logic of DFA for changing the state or Configuration
 modifyConf::Conf->Conf
 modifyConf c = case c of
  Inter Init st -> case parse parseName (st ^. remainingInputL) of
@@ -56,13 +61,15 @@ modifyConf c = case c of
                                         _->Inter Init .gotoInitState $ st
                    Left _ -> Inter Init . gotoInitState $ st
 
-
+-- | gives the head character of the remainingInput of a given Mystate
 getHead::Mystate->Char
 getHead st = head $ st ^. remainingInputL
 
+-- | drops or consumes the head of the remainingInput of a given Mystate
 removeHead::Mystate->Mystate
 removeHead =  (%~) remainingInputL (drop 1)
 
+-- | sets the currName and currWeigth field to Nothing of a given Mystate 
 gotoInitState::Mystate->Mystate
 gotoInitState st = st & (currNameL .~ Nothing).(currWeigthL .~ Nothing)
 
@@ -70,30 +77,35 @@ gotoInitState st = st & (currNameL .~ Nothing).(currWeigthL .~ Nothing)
 endOfInput::Mystate->Bool
 endOfInput st = st ^. remainingInputL == ""
 
-
+-- | A parser for name i.e only alphabets are accepted
 parseName::ReadP String
 parseName = many1 (satisfy isLetter)
 
+-- | parses one or more spaces
 parseSpaces::ReadP String
 parseSpaces = many1 (char ' ')
 
+-- | A float parser
 parseFloat::ReadP Float
 parseFloat = fmap read $ (++) <$> number <*> decimal
  where
   decimal = option "" $ (:) <$> char '.' <*> number
   number = many1 (satisfy isDigit)
 
+-- | parses zero or more spaces followed by "kg" keyword
 parseKg::ReadP String
 parseKg = do
      many (char ' ')
      string "kg"
 
+-- | A function to wrap the output of parser with Either
 parse::ReadP a-> String-> Either String (a,String)
 parse p s = case readP_to_S p s of
  [] -> Left s
  xs -> Right (last xs)
 
-
+-- | gives list of persons from a given Conf if 
+--   its (Final something) else emptylist
 getPersons::Conf->[Person]
 getPersons (Final st) = st ^. personsL 
 getPersons _ = []
@@ -103,6 +115,8 @@ getPersons _ = []
 makeInitConf::String->Conf
 makeInitConf = Inter Init . Mystate mempty mempty Nothing 
 
+-- | validates a Person according to given conditions of 
+--   minimum and maximum weights
 validate::[Person]->[Person]
 validate = filter (\(Person _ w) -> w >= minWeight && w<= maxWeight)
 
